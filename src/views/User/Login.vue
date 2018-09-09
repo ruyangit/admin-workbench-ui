ae35cfc34cc74e900506412c25e3b95843fb7ea2
ruyangit <ruyangit@163.com>
2018-09-09 08:55:34 +0800

    edit:Login

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -1,83 +1,21 @@
-<template>
-<div class="ai-view-login-main">
-    <Login @change="onSwitchTab">
-      <Tab key="1" tab="账户密码登录">
-        <FormItem fieldDecoratorId="username" :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please input your Username!' }]}">
-          <a-input placeholder="Username" size="large">
-            <Icon slot="prefix" type="user" />
-          </a-input>
-        </FormItem>
-        <FormItem fieldDecoratorId="password" :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please input your Password!' }]}">
-          <a-input placeholder="Password" size="large">
-            <Icon slot="prefix" type="lock" />
-          </a-input>
-        </FormItem>
-      </Tab>
-      <Tab key="2" tab="手机号登录">
-        <FormItem fieldDecoratorId="mobile" :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please input your mobile!' }]}">
-          <a-input placeholder="Mobile/number" size="large">
-            <Icon slot="prefix" type="mobile" />
-          </a-input>
-        </FormItem>
-        <FormItem fieldDecoratorId="captcha" :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please input your captcha!' }]}">
-          <a-row :gutter="8">
-            <a-col :span="16">
-              <a-input placeholder="Captcha/number" size="large">
-                <Icon slot="prefix" type="scan" />
-              </a-input>
-            </a-col>
-            <a-col :span="8">
-              <Button size="large" class="getCaptcha">
-               获取验证码
-              </Button>
-            </a-col>
-          </a-row>
-        </FormItem>
-      </Tab>
-      <div>
-        <Checkbox :checked="autoLogin" @change="onSwithAutoLogin">
-          自动登录
-        </Checkbox>
-        <a style="float:right;" href="#">忘记密码</a>
-      </div>
-      <Submit :loading="submitting">登录</Submit>
-      <div class="other">
-        其他登录方式
-        <Icon class="icon" type="alipay-circle" />
-        <Icon class="icon" type="taobao-circle" />
-        <Icon class="icon" type="weibo-circle" />
-        <a class="register" href="#">
-              注册账户
-            </a>
-      </div>
-    </Login>
-</div>
-</template>
-
 <script>
-import Login from "@/components/Login";
-import { Form, Input, Icon, Checkbox, Row, Col, Button } from "ant-design-vue";
-const { Tab, Submit } = Login;
+import {
+  Form,
+  Tabs,
+  Input,
+  Icon,
+  Checkbox,
+  Row,
+  Col,
+  Button
+} from "ant-design-vue";
 
-export default {
+const AiLogin = {
   data: () => ({
+    defaultActiveKey: "1",
     submitting: false,
-    autoLogin: false,
-    form: {}
+    autoLogin: false
   }),
-  components: {
-    Login,
-    Tab,
-    Submit,
-    Form,
-    FormItem: Form.Item,
-    "a-input": Input,
-    Icon,
-    Checkbox,
-    "a-row": Row,
-    "a-col": Col,
-    Button
-  },
   methods: {
     onSwithAutoLogin(e) {
       this.autoLogin = e.target.checked;
@@ -92,8 +30,146 @@ export default {
         }
       });
     }
+  },
+  render() {
+    const { getFieldDecorator } = this.form;
+    return (
+      <div class="ai-view-login-main">
+        <div class="ai-login">
+          <Form onSubmit={this.handleSubmit}>
+            <Tabs
+              animated={false}
+              onChange={this.onSwitchTab}
+              defaultActiveKey={this.defaultActiveKey}
+            >
+              <Tabs.TabPane key="1" tab="账户密码登录">
+                <Form.Item>
+                  {getFieldDecorator("username", {
+                    rules: [
+                      { required: true, message: "Please input your Username!" }
+                    ]
+                  })(
+                    <Input
+                      prefix={
+                        <Icon
+                          type="user"
+                          style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                      }
+                      placeholder="admin/user"
+                      size="large"
+                    />
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator("password", {
+                    rules: [
+                      { required: true, message: "Please input your Password!" }
+                    ]
+                  })(
+                    <Input
+                      prefix={
+                        <Icon
+                          type="lock"
+                          style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                      }
+                      type="password"
+                      placeholder="000000"
+                      size="large"
+                    />
+                  )}
+                </Form.Item>
+              </Tabs.TabPane>
+              <Tabs.TabPane key="2" tab="手机号登录">
+                <Form.Item>
+                  {getFieldDecorator("mobile", {
+                    rules: [
+                      { required: true, message: "Please input your Mobile!" }
+                    ]
+                  })(
+                    <Input
+                      prefix={
+                        <Icon
+                          type="mobile"
+                          style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                      }
+                      placeholder="138000000"
+                      size="large"
+                    />
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  <Row gutter={8}>
+                    <Col span={16}>
+                      {getFieldDecorator("captcha", {
+                        rules: [
+                          {
+                            required: true,
+                            message: "Please input your Captcha!"
+                          }
+                        ]
+                      })(
+                        <Input
+                          prefix={
+                            <Icon
+                              type="scan"
+                              style={{ color: "rgba(0,0,0,.25)" }}
+                            />
+                          }
+                          placeholder="0000"
+                          size="large"
+                        />
+                      )}
+                    </Col>
+                    <Col span={8}>
+                      <Button size="large" class="getCaptcha">
+                        获取验证码
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Item>
+              </Tabs.TabPane>
+            </Tabs>
+            <div>
+              <Checkbox
+                checked={this.autoLogin}
+                onChange={this.onSwithAutoLogin}
+              >
+                自动登录
+              </Checkbox>
+              <a style="float:right;" href="#">
+                忘记密码
+              </a>
+            </div>
+            <Form.Item>
+              <Button
+                size="large"
+                class="submit"
+                type="primary"
+                htmlType="submit"
+              >
+                登录
+              </Button>
+            </Form.Item>
+            <div class="other">
+              其他登录方式
+              <Icon class="icon" type="alipay-circle" />
+              <Icon class="icon" type="taobao-circle" />
+              <Icon class="icon" type="weibo-circle" />
+              <a class="register" href="#">
+                注册账户
+              </a>
+            </div>
+          </Form>
+        </div>
+      </div>
+    );
   }
 };
+
+export default Form.create()(AiLogin);
 </script>
 
 <style lang="less">
